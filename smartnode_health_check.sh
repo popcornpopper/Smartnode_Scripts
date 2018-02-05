c800eace78d36bfad1e1e50ef5841c24a530876b
popcornpopper <36144464+popcornpopper@users.noreply.github.com>
2018-02-04 20:49:33 -0800

    Added description , author creds

--- smartnode_health_check.sh
+++ smartnode_health_check.sh
@@ -1,11 +1,20 @@
-
+#!/bin/bash
+#
+#  Smartnode Health Check script
+#
+#  Quickly determine the root cause of Smartnode issues.
+#  Assumptions: This script is intended to be run on a completely configured Smartnode.
+#               Not intended for incomplete Smartnode Installations.
+#
+#
+#  Author : popcornpopper ( bsmart ) . Created Feb 4, 2018
 
 PATH=/bin:/usr/bin:/sbin:$PATH
 export PATH
 
 check_disk_space () {
 
-   
+
    FAIL=0
    IFS=\$
    for fspct in `df -k  |grep -v ^Filesystem | awk '{ print $5" "$4" "$6"\$" }' | sort -nr | tr -d '\012' `
@@ -23,9 +32,9 @@ check_disk_space () {
       fi
    fi
 
-  
+
    sizemb=`perl -e "use POSIX qw(round);print round(( $size / 1000 ))"`
-   if [ $FAIL -eq 1 ];then 
+   if [ $FAIL -eq 1 ];then
          echo "FAIL: $fs fs has ${sizemb}Megs(${pct}% used) , this has breached thresholds of (must be < ${FREE_PCT_THRESHOLD}% + >12GB)"
    else  echo "OK: $fs fs has ${sizemb}Megs(${pct}% used) , within thresholds (must be < ${FREE_PCT_THRESHOLD}% + >12GB)"
    fi
@@ -46,14 +55,14 @@ check_cmd_pattern() {
 
    if [ -z "$prefilter" ] ; then
        $command  |grep -v grep | grep -v ^\# | grep -w "${pattern1}" > /dev/null 2>&1
-   else 
+   else
        $command  |grep -v grep | grep -v ^\# | grep -w "${prefilter}"  |grep -w "$pattern1" > /dev/null 2>&1
    fi
    ret=$?
 
-   if [ $ret -eq 0 ]; then 
+   if [ $ret -eq 0 ]; then
       echo "OK: $desc : Test successful for $pattern1 " ; return 0;
-   else 
+   else
       echo "FAIL: $desc : Test failed for  $pattern1 "
       echo "     This test failed : $command | grep -w \"${pattern1}\"  "
       return 1
@@ -79,12 +88,12 @@ check_cmd_pattern "ps -ef" "Check if smartcashd daemon process is running" smart
 check_sc_status () {
   check_cmd_pattern  'smartcash-cli smartnode status' "Check smartcash-cli smartnode status" "Smartnode successfully started"
 }
- 
+
 check_sc_port () {
   if [ "`whoami`" != root ]; then
         check_cmd_pattern 'sudo iptables -L' "Check if your network allows traffic to SN port" "dpt:9678" "ACCEPT"
   else check_cmd_pattern 'iptables -L' "Check if your network allows traffic to SN port" "dpt:9678" "ACCEPT";
-  fi 
+  fi
   check_cmd_pattern 'netstat -an' "Check if SN daemon port is listening" "9678" "LISTEN"
 }
 
@@ -102,11 +111,11 @@ check_system_stats() {
 
 mtotal="`free -m  |grep ^Mem| awk '{ print $2 }'|tr -d '\012'`"
 mavail="`free -m |grep ^Mem | awk '{ print $7 }'|tr -d '\012'`"
-mpct=`perl -e "use POSIX qw(round);print round(100*( $mavail / $mtotal ))"` 
+mpct=`perl -e "use POSIX qw(round);print round(100*( $mavail / $mtotal ))"`
 
 if [ $mpct -lt $MEM_THRESHOLD ]; then
-     echo "OK: Mem usage ${mpct}% under threshold(${MEM_THRESHOLD}%)" 
-else echo "FAIL: Mem usage ${mpct}% above threshold(${MEM_THRESHOLD}%)" 
+     echo "OK: Mem usage ${mpct}% under threshold(${MEM_THRESHOLD}%)"
+else echo "FAIL: Mem usage ${mpct}% above threshold(${MEM_THRESHOLD}%)"
 fi
 
 cpua=0
@@ -117,10 +126,10 @@ done
   cpuavg=`perl -e "use POSIX qw(round);print 100-round(( $cpua / 5 ))"`
 
 if [ $cpuavg -lt $CPU_THRESHOLD ]; then
-     echo "OK: CPU usage ${cpuavg}% under threshold(${CPU_THRESHOLD}%)" 
-else echo "FAIL: CPU usage ${cpuavg}% above threshold(${CPU_THRESHOLD}%)" 
+     echo "OK: CPU usage ${cpuavg}% under threshold(${CPU_THRESHOLD}%)"
+else echo "FAIL: CPU usage ${cpuavg}% above threshold(${CPU_THRESHOLD}%)"
 fi
-   
+
 }
 
 ####### MAIN #############
@@ -135,7 +144,7 @@ FREE_SIZE_THRESHOLD=12000000  # 12GB
 
 
 echo "
-SmartNode Health Check Analysis report 
+SmartNode Health Check Analysis report
 ---------------------------------------------------------
 "
 
