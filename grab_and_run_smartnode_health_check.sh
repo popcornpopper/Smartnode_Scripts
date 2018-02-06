5c36e591f3b1957ca30427a6ca0246d4a08785ef
popcornpopper <36144464+popcornpopper@users.noreply.github.com>
2018-02-05 21:14:00 -0800

    Update grab_and_run_smartnode_health_check.sh

--- grab_and_run_smartnode_health_check.sh
+++ grab_and_run_smartnode_health_check.sh
@@ -1,4 +1,9 @@
 # Wrapper script
+#
+# Instructions:
+# Run below command:
+#    1. wget https://raw.githubusercontent.com/popcornpopper/Smartnode_Scripts/master/grab_and_run_smartnode_health_check.sh
+#    2. sh grab_and_run_smartnode_health_check.sh
 
 mkdir -p /tmp/smartnode_health_check
 rm -f  /tmp/smartnode_health_check/smartnode_health_check.sh 2> /dev/null
