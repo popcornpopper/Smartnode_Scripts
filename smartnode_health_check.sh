931873b6e7a5da335546318403520e2adc4a0fc5
popcornpopper <36144464+popcornpopper@users.noreply.github.com>
2018-02-05 18:43:17 -0800

    Update smartnode_health_check.sh

--- smartnode_health_check.sh
+++ smartnode_health_check.sh
@@ -179,9 +179,7 @@ echo "
 SmartNode Health Check Analysis report ( running as `whoami` )
 --------------------------------------------------------------"
 if [ "`whoami`" = "smartadmin" ]; then echo "NOTE: You maybe prompted to input sudo password to run iptables command"; fi
-echo "
-
-"
+echo 
 
 ########## Performing Smartcashd Tests ##################"
 check_smartcashd_process
