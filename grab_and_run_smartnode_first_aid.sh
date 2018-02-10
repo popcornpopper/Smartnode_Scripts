9ce7143dadaec1cc02b426a7b5ec2549102f42ab
popcornpopper <36144464+popcornpopper@users.noreply.github.com>
2018-02-10 11:26:59 -0800

    Update and rename grab_and_run_smartnode_health_check.sh to grab_and_run_smartnode_first_aid.sh

---
+++ grab_and_run_smartnode_first_aid.sh
@@ -0,0 +1,14 @@
+# Wrapper script
+#
+# Instructions: Download this script only once. This will not change.
+# Run below command:
+#    1. wget https://raw.githubusercontent.com/popcornpopper/Smartnode_Scripts/master/grab_and_run_smartnode_health_check.sh
+#    2. sh grab_and_run_smartnode_health_check.sh
+
+mkdir -p /tmp/smartnode_first_aid
+rm -f  /tmp/smartnode_first_aid/smartnode_first_aid.sh 2> /dev/null
+wget -O /tmp/smartnode_first_aid/smartnode_first_aid.sh https://raw.githubusercontent.com/popcornpopper/Smartnode_Scripts/master/smartnode_first_aid.sh
+bash /tmp/smartnode_first_aid/smartnode_first_aid.sh | tee /tmp/smartnode_health_check/smartnode_first_aid.txt
+
+echo "Smartnode First Aid Report : /tmp/smartnode_health_check/smartnode_first_aid_report.txt"
+echo
