bff5e7585bed22288a870930ccffb208f0292e90
popcornpopper <36144464+popcornpopper@users.noreply.github.com>
2018-02-10 15:07:46 -0800

    Update grab_and_install_smartnode_crons.sh

--- grab_and_install_smartnode_crons.sh
+++ grab_and_install_smartnode_crons.sh
@@ -14,50 +14,63 @@
 # Created: Feb 10, 2018
 #
 
-if [ $# -ne 1 ]
-then
-   echo "USAGE: sh setup_cron.sh [Smartnode Dir]"
-   exit 1
-fi
+clear
 
-SMARTNODE_DIR="$1"
+SMARTCASH_CONF="`ls -1d ~/.smartcash/smartcash.conf`"
+SMARTCASH_DIR="`dirname $SMARTCASH_CONF`"
+SMARTCASH_BASE="`dirname $SMARTCASH_DIR`"
 
 ## Make sure Smartnode DIR exists
-if [ ! -s ${SMARTNODE_DIR}/.smartcash/smartcash.conf ]
+if [ ! -s ${SMARTCASH_BASE}/.smartcash/smartcash.conf ]
 then
-   echo "ERROR: Invalid Smartnode directory ${SMARTNODE_DIR}. "
+   echo
+   echo "ERROR: Unable to locate Smartnode base directory . ${SMARTCASH_BASE}/.smartcash does not exist.
+    - If you installed Smartnode using the Setup Guides with smartadmin , please login as smartadmin user.
+    - If you installed Smartnode using bash installer, please login as root"
    exit 99
+else
+   echo
+   echo "OK : detected ${SMARTCASH_DIR} as your smartcash top directory"
+   echo "OK : detected ${SMARTCASH_BASE} as your smartcash base directory"
+   echo
+   echo "Using account `whoami` to cron-schedule the official Smartcash Smartnode Scripts"
+   echo
 fi
 
-## If they exist, make backup of ${SMARTNODE_DIR}/smartnode and original cron scheds. User can revert back to these if needed.
-if [ ! -d ${SMARTNODE_DIR}/smartnode_prev ]; then cp -Rp ${SMARTNODE_DIR}/smartnode ${SMARTNODE_DIR}/smartnode_prev; fi
+echo "
+To proceed with downloading and scheduling Smartcash's official Smartnode scripts,
+please press ENTER key, to cancel press CTRL-C"
+read go_no_go
+
+## If they exist, make backup of ${SMARTCASH_BASE}/smartnode and original cron scheds. User can revert back to these if needed.
+if [ ! -d ${SMARTCASH_BASE}/smartnode_prev -a -d ${SMARTCASH_BASE}/smartnode ]; then cp -Rp ${SMARTCASH_BASE}/smartnode ${SMARTCASH_BASE}/smartnode_prev; fi
 if [ ! -f /tmp/crontab.prev ]; then crontab -l > /tmp/crontab.prev ; fi
 
 ## Prepwork
-mkdir -p ${SMARTNODE_DIR}/smartnode
-rm -f ${SMARTNODE_DIR}/smartnode/makerun.sh
-rm -f ${SMARTNODE_DIR}/smartnode/checkdaemon.sh
-rm -f ${SMARTNODE_DIR}/smartnode/upgrade.sh
-rm -f ${SMARTNODE_DIR}/smartnode/clearlog.sh
+mkdir -p ${SMARTCASH_BASE}/smartnode
+rm -f ${SMARTCASH_BASE}/smartnode/makerun.sh
+rm -f ${SMARTCASH_BASE}/smartnode/checkdaemon.sh
+rm -f ${SMARTCASH_BASE}/smartnode/upgrade.sh
+rm -f ${SMARTCASH_BASE}/smartnode/clearlog.sh
 
-# Downloading the official smartnode maintenance scripts 
-wget -O ${SMARTNODE_DIR}/smartnode/makerun.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/makerun.sh 2>/dev/null
-wget -O ${SMARTNODE_DIR}/smartnode/checkdaemon.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/checkdaemon.sh 2>/dev/null
-wget -O ${SMARTNODE_DIR}/smartnode/upgrade.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh 2>/dev/null
-wget -O ${SMARTNODE_DIR}/smartnode/clearlog.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/clearlog.sh 2>/dev/null
+# Downloading the official smartnode maintenance scripts
+wget -O ${SMARTCASH_BASE}/smartnode/makerun.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/makerun.sh 2>/dev/null
+wget -O ${SMARTCASH_BASE}/smartnode/checkdaemon.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/checkdaemon.sh 2>/dev/null
+wget -O ${SMARTCASH_BASE}/smartnode/upgrade.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh 2>/dev/null
+wget -O ${SMARTCASH_BASE}/smartnode/clearlog.sh https://raw.githubusercontent.com/SmartCash/smartnode/master/clearlog.sh 2>/dev/null
 
 
 ## Giving the scripts locked down read, write and execute permissions
-chmod 700 ${SMARTNODE_DIR}/smartnode/*.sh
+chmod 700 ${SMARTCASH_BASE}/smartnode/*.sh
 
 ## Below will create a temp file to store the smartnode cron scripts
 echo "
 @reboot /usr/bin/smartcashd > /tmp/start_at_reboot.out 2>&1
 
-*/1 * * * * ${SMARTNODE_DIR}/smartnode/makerun.sh > /tmp/makerun.sh.out 2>&1
-*/30 * * * * ${SMARTNODE_DIR}/smartnode/checkdaemon.sh > /tmp/checkdaemon.sh.out 2>&1
-*/120 * * * * ${SMARTNODE_DIR}/smartnode/upgrade.sh > /tmp/upgrade.sh.out 2>&1
-0 8,20 * * * ${SMARTNODE_DIR}/smartnode/clearlog.sh > /tmp/clearlog.sh.out 2>&1
+*/1 * * * * ${SMARTCASH_BASE}/smartnode/makerun.sh > /tmp/makerun.sh.out 2>&1
+*/30 * * * * ${SMARTCASH_BASE}/smartnode/checkdaemon.sh > /tmp/checkdaemon.sh.out 2>&1
+*/120 * * * * ${SMARTCASH_BASE}/smartnode/upgrade.sh > /tmp/upgrade.sh.out 2>&1
+0 8,20 * * * ${SMARTCASH_BASE}/smartnode/clearlog.sh > /tmp/clearlog.sh.out 2>&1
 
 " > /tmp/smartnode.crontabs
 
@@ -74,7 +87,7 @@ echo
 echo "If you want to revert back , run below commands in the following order :
   1.  crontab /tmp/crontab.prev
   2.  rm -f /tmp/crontab.prev
-  3.  cp -p ${SMARTNODE_DIR}/smartnode_prev/*.sh  ${SMARTNODE_DIR}/smartnode 2> /dev/null
-  4.  rmdir ${SMARTNODE_DIR}/smartnode_prev/
+  3.  mv ${SMARTCASH_BASE}/smartnode_prev/*.sh  ${SMARTCASH_BASE}/smartnode 2> /dev/null
+  4.  rmdir ${SMARTCASH_BASE}/smartnode_prev/
 "
 
