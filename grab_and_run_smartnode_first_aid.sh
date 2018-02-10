ea3b405d10e2a14b0223b116d9a33cded4df3e6a
popcornpopper <36144464+popcornpopper@users.noreply.github.com>
2018-02-10 11:27:23 -0800

    Update grab_and_run_smartnode_first_aid.sh

--- grab_and_run_smartnode_first_aid.sh
+++ grab_and_run_smartnode_first_aid.sh
@@ -2,8 +2,8 @@
 #
 # Instructions: Download this script only once. This will not change.
 # Run below command:
-#    1. wget https://raw.githubusercontent.com/popcornpopper/Smartnode_Scripts/master/grab_and_run_smartnode_health_check.sh
-#    2. sh grab_and_run_smartnode_health_check.sh
+#    1. wget https://raw.githubusercontent.com/popcornpopper/Smartnode_Scripts/master/grab_and_run_smartnode_first_aid.sh
+#    2. sh grab_and_run_smartnode_first_aid.sh
 
 mkdir -p /tmp/smartnode_first_aid
 rm -f  /tmp/smartnode_first_aid/smartnode_first_aid.sh 2> /dev/null
